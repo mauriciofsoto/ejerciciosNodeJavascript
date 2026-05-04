2b5d3695a7197c9cc82d4dda1bf710b6ab4a1be2
Stackft <mauriciofst04@gmail.com>
2026-05-03 21:08:10 -0300

    punto 3 finalizado

--- miIndex.js
+++ miIndex.js
@@ -1,6 +1,8 @@
 const {validarSaldo, realizarTransferencia} = require("./cuentaBancaria");
 const {validarCorrelativas, inscribirAMaterias, alumno} = require("./instituto");
+const {validarStock, realizarVenta, imprimirEtiqueta, productos} = require("./mercadoRestringido")
 
+// Ejercicio 1
 validarSaldo(20000, 27800)
     .then((respuesta) => {
         console.log(respuesta);
@@ -16,6 +18,8 @@ validarSaldo(20000, 27800)
                 console.log("proceso finalizado")
             })
 
+
+//ejercicio 2
 validarCorrelativas(alumno)
     .then((respuesta) => {
         console.log(respuesta);
@@ -30,3 +34,23 @@ validarCorrelativas(alumno)
                 .finally(() =>{
                     console.log("proceso finalizado")
                 })
+
+// Ejercicio 3
+validarStock("mesa", productos)
+                .then((respuesta) =>{
+                    console.log(respuesta);
+                    return realizarVenta(respuesta)
+                })
+                    .then((respuesta) =>{
+                        console.log(respuesta);
+                        return imprimirEtiqueta(respuesta)
+                    })
+                        .then((respuesta) =>{
+                            console.log(respuesta);
+                        })
+                            .catch((error) =>{
+                                console.log(error);
+                            })
+                                .finally(() =>{
+                                    console.log("Proceso finalizado");
+                                })
